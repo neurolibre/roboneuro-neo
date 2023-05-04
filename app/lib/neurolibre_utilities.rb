69fe3f5b6d04038f0795b2a729c2d6744d82e1ed
Agah <agahkarakuzu@gmail.com>
2023-05-03 20:50:01 -0400

    Teach GPT manners as I am resolving a server-side problem

--- app/lib/neurolibre_utilities.rb
+++ app/lib/neurolibre_utilities.rb
@@ -51,7 +51,7 @@ module NeurolibreUtilities
         response = openai_client.chat(
             parameters: {
                 model: "gpt-3.5-turbo", # Required.
-                messages: [{ role: "user", content: "Write an amusing quote based on the content of https://raw.githubusercontent.com/#{target_repo.split("/")[0]}/#{target_repo.split("/")[1]}/#{branch}/paper.md"}], # Required.
+                messages: [{ role: "user", content: "Write an amusing and flattering quote based on the content of https://raw.githubusercontent.com/#{target_repo.split("/")[0]}/#{target_repo.split("/")[1]}/#{branch}/paper.md"}], # Required.
                 temperature: 0.7,
             })
         return response.dig("choices", 0, "message", "content")
