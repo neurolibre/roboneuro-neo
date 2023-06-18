a0f1e49c1accaf40b9048868e84f5f8653a1b18e
Agah <agahkarakuzu@gmail.com>
2023-06-18 01:35:30 -0400

    Add sync pdf endpoint

---
+++ app/responders/neurolibre/preprint_sync_pdf_responder.rb
@@ -0,0 +1,61 @@
+require_relative "../../lib/responder"
+
+module Neurolibre
+class PreprintSyncPdfResponder < Responder
+
+  keyname :neurolibre_sync_pdf
+
+  def define_listening
+    required_params :external_call
+    @event_action = "issue_comment.created"
+    @event_regex = /\A@#{bot_name} production sync pdf\.?\s*$/i
+  end
+
+  def process_message(message)
+    return unless roles_and_issue?
+    process_external_service(params[:external_call], locals)
+  end
+
+  def roles_and_issue?
+    unless username?(reviewers_usernames.first.to_s)
+      respond("Can't perform this without reviewers")
+      return false
+    end
+
+    unless username?(editor_username)
+      respond("Can't perform this without an editor")
+      return false
+    end
+
+    true
+  end
+
+  def reviewers_usernames
+    @reviewers_usernames ||= read_value_from_body("reviewers-list").split(",").map(&:strip)
+  end
+
+  def reviewers_logins
+    @reviewers_logins ||= reviewers_usernames.map {|reviewer_username| user_login(reviewer_username)}.join(",")
+  end
+
+  def editor_username
+    @editor_username ||= read_value_from_body("editor")
+  end
+
+  def editor_login
+    @editor_login ||= user_login(editor_username)
+  end
+
+  def title_regex
+    params[:review_title_regex] || /^\[REVIEW\]:/
+  end
+
+  def default_description
+    "Copy summary PDF from the preprints repository to the NeuroLibre production server."
+  end
+
+  def default_example_invocation
+    "@#{bot_name} production sync pdf"
+  end
+end
+end
